379751d54499e13ed9fe880e6e96b3b0603f0eca
NwobiaDavid <dnwobia@gmail.com>
2023-04-30 16:03:15 +0100

    v1 --- done

---
+++ app.js
@@ -0,0 +1,28 @@
+$(window).scroll(startCounter);
+
+function startCounter() {
+  if ($(window).scrollTop() > $('.counter').offset().top - $(window).height()) {
+    $(window).off("scroll", startCounter);
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+          }
+
+        });
+    });
+  }
+}
